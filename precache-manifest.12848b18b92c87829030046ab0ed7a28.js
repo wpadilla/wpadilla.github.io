9e860c68c0c34a36e6b68d37d5d094627c11dbfe
Williams <wilfre_padilla@hotmail.com>
2020-11-30 11:46:01 -0400

    changing api to vercel serverless

---
+++ precache-manifest.12848b18b92c87829030046ab0ed7a28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59972fe59c960e3d6055d11d7e984e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02df6cdf8eceb42a4f7",
+    "url": "/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "52813024fe0d05cb9ff0",
+    "url": "/static/css/main.190ae3e7.chunk.css"
+  },
+  {
+    "revision": "b02df6cdf8eceb42a4f7",
+    "url": "/static/js/2.f41ca7c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f41ca7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52813024fe0d05cb9ff0",
+    "url": "/static/js/main.5bb911bb.chunk.js"
+  },
+  {
+    "revision": "22f6a4c3832221d8505e",
+    "url": "/static/js/runtime-main.55916493.js"
+  },
+  {
+    "revision": "7d433192b06a228ae5a0b633fc215838",
+    "url": "/static/media/logo.7d433192.png"
+  }
+]);
